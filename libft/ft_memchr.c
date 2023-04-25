09d746c0dbbc2f111005feb702c1e8000c723ec7
farkasf <120285330+farkasf@users.noreply.github.com>
2023-04-25 21:49:24 +0200

    Libft_v2.0 updated

--- libft/ft_memchr.c
+++ libft/ft_memchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ffarkas <ffarkas@student.42prague.com      +#+  +:+       +#+        */
+/*   By: ffarkas <ffarkas@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 02:45:51 by ffarkas           #+#    #+#             */
-/*   Updated: 2023/01/14 02:52:00 by ffarkas          ###   ########.fr       */
+/*   Updated: 2023/04/25 21:44:44 by ffarkas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	*ft_memchr(const void *buf, int ch, size_t length)
 	while (i < length)
 	{
 		if (((unsigned char *)buf)[i] == (unsigned char)ch)
-			return ((char *) &buf[i]);
+			return ((void *)&((char *)buf)[i]);
 		i++;
 	}
 	return (NULL);
